c42042f7f7197ab21c0f097fdc79d533c081c43a
Jennifer Magana <jennmagana@gmail.com>
2020-05-06 16:48:02 -0700

    created handleGetGradesError and handleGetGradesSucess methods

---
+++ components/app.js
@@ -0,0 +1,33 @@
+class App {
+  constructor() {
+
+  }
+  handleGetGradesError(error) {
+    console.error(error)
+  },
+  handleGetGradesSuccess(grades) {
+    console.log(grades)
+  }
+
+}
+
+
+
+
+
+
+
+// class APP {
+//   constructor() {
+
+//   }
+
+//   handleGetGradesError(error) {
+//     console.error(error);
+//   },
+
+//   handleGetGradesSuccess(grades) {
+//     console.log(grades);
+//   },
+
+// }
