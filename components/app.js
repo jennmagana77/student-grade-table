d9ca53664b336ed85c8cbccb30ea1aa31ba18c5d
Jennifer Magana <jennmagana@gmail.com>
2020-05-06 17:10:58 -0700

    added getGrades and start methods, instantiated App class

--- components/app.js
+++ components/app.js
@@ -1,12 +1,25 @@
 class App {
   constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
 
   }
   handleGetGradesError(error) {
     console.error(error)
-  },
+  }
   handleGetGradesSuccess(grades) {
     console.log(grades)
+  }
+  getGrades() {
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      headers: {
+        "X-Access-Token": "yWCk5Gwx"
+      },
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError,
+    })
   }
 
 }
